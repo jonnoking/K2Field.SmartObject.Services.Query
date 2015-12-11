e3929126f25002c7fc1480451a3e171ca1550ebd
jonnoking <jonno@jonnoking.com>
2015-12-11 07:49:04 -0800

    0.0.0.1

---
+++ K2Field.SmartObject.Services.Query/Data/ServiceConfigurationSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace K2Field.SmartObject.Services.Query.Data
+{
+    public class ServiceConfigurationSettings
+    {
+        public static readonly string ConnectionString = "ConnectionString";
+        public static readonly string Namespace = "Namespace";
+        public static readonly string Issuer = "Issuer";
+        public static readonly string Key = "Key";
+        public static readonly string DynamicDiscovery = "DynamicDiscovery";
+        public static readonly string Delimiter1 = "Delimiter1";
+        public static readonly string Delimiter2 = "Delimiter2";
+        public static readonly string RequestTimeout = "RequestTimeout";
+    }
+}
---
+++ K2Field.SmartObject.Services.Query/Utilities/ServiceUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SourceCode.SmartObjects.Services.ServiceSDK;
+using SourceCode.SmartObjects.Services.ServiceSDK.Objects;
+using K2Field.SmartObject.Services.Query.Data;
+
+namespace K2Field.SmartObject.Services.Azure.ServiceBus.Utilities
+{
+    public class ServiceUtilities
+    {
+        private ServiceAssemblyBase serviceBroker = null;       
+
+        public ServiceUtilities(ServiceAssemblyBase serviceBroker)
+        {
+            // Set local serviceBroker variable.
+            this.serviceBroker = serviceBroker;
+        }        
+    }
+}
